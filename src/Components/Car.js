1ff186b41d068e24e7ae6cc9d9e59d2d85b62aaa
Shagun Mengi <shagunmengi003@gmail.com>
2023-01-19 00:22:52 +0530

    box

--- src/Components/Car.js
+++ src/Components/Car.js
@@ -1,13 +1,62 @@
 import React from "react";
-
-
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 export default function Car(){
     
+
+// init
+
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animation );
+document.body.appendChild( renderer.domElement );
+
+//stars
+function addStar(){
+  const geometry = new THREE.SphereGeometry( 0.25, 32, 16 );
+const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } );
+const sphere = new THREE.Mesh( geometry, material );
+
+const [x,y,z] = Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100));
+
+sphere.position.set(x,y,z);
+scene.add( sphere );
+}
+
+Array(200).fill().forEach(addStar);
+
+
+// animation
+
+function animation( time ) {
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+{/* <Stars/> */}
+	renderer.render( scene, camera );
+
+}
+
     return(
 
 <>
-<p style={{color:"white"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus possimus accusamus officiis, vitae nobis rem natus itaque? Ut ipsum </p><p style={{color:"white"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus possimus accusamus officiis, vitae nobis rem natus itaque? Ut ipsum </p>
-
+<p style={{color:"white"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus possimus accusamus officiis, vitae nobis rem natus itaque? Ut ipsum </p>
+{/* <Canvas>
+		<Stars />
+	</Canvas> */}
 </>
        
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars } from "@react-three/drei";
-import { Physics, usePlane, useBox } from "@react-three/cannon";
 import "./styles.css";
 import Car from "./Components/Car"
 import NavBar from "./Components/navbar"
@@ -16,17 +13,13 @@ createRoot(document.getElementById('root')).render(
 	<h1 style={{color:'white'}}>carrrr</h1>
 
 	<Car/>
-	<Car/>
-	<Car/>
-	<Car/>
 
 
 		<main>
 		<p style={{color:"white"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus possimus accusamus officiis, vitae nobis rem natus itaque? Ut ipsum </p>
 		<p style={{color:"white"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus possimus accusamus officiis, vitae nobis rem natus itaque? Ut ipsum </p>
 		<p style={{color:"white"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus possimus accusamus officiis, vitae nobis rem natus itaque? Ut ipsum </p>
-	
 		 </main>
-<BoxGeo/>
+{/* <BoxGeo/> */}
 	</>
 );
